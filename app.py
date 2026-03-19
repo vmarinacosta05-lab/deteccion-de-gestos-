d50f7fe0659a5c759061e3f9746a792a326aaadd
vmarinacosta05-lab <vmarinacosta05@gmail.com>
2026-03-19 13:54:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ if img_file_buffer is not None:
     # run the inference
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
+    if prediction[0][0]>0.9:
+      st.header('HOLA VALE')
+    if prediction[0][1]>0.2:
+      st.header('HOLA SALO')
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
